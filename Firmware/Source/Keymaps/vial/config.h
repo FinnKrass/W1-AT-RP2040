72d5dda43f29b68cacec3a7e1833a6fcc5badf58
FinnKrass <finnkrassoi@gmail.com>
2023-07-25 02:50:26 +1000

    firmware and proto 1

---
+++ Firmware/Source/Keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDF, 0x3D, 0x30, 0x26, 0x88, 0x85, 0xB7, 0x67}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0}
+#define VIAL_UNLOCK_COMBO_COLS {2}
